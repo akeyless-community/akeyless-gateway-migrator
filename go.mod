2b66902ea5963ef8f0c61d06783d2b5352501a24
Chris Gruel <chris.g@akeyless.io>
2023-10-16 12:51:43 -0400

    add lookup of k8s auth configs

--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.21.2
 require github.com/akeylesslabs/akeyless-go/v2 v2.20.3
 
 require (
+	github.com/gojek/heimdall v5.0.2+incompatible // indirect
+	github.com/gojektech/heimdall v5.0.2+incompatible // indirect
+	github.com/gojektech/valkyrie v0.0.0-20190210220504-8f62c1e7ba45 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
 
--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/gojek/heimdall/httpclient"
 	"github.com/spf13/cobra"
 )
+
+type KubeAuthConfig struct {
+	Name                 string `json:"name,omitempty"`
+	ID                   string `json:"id,omitempty"`
+	ProtectionKey        string `json:"protection_key,omitempty"`
+	AuthMethodAccessID   string `json:"auth_method_access_id,omitempty"`
+	AuthMethodPrvKeyPem  string `json:"auth_method_prv_key_pem,omitempty"`
+	AmTokenExpiration    int    `json:"am_token_expiration,omitempty"`
+	K8SHost              string `json:"k8s_host,omitempty"`
+	K8SCaCert            string `json:"k8s_ca_cert,omitempty"`
+	K8STokenReviewerJwt  string `json:"k8s_token_reviewer_jwt,omitempty"`
+	K8SIssuer            string `json:"k8s_issuer,omitempty"`
+	K8SPubKeysPem        string `json:"k8s_pub_keys_pem,omitempty"`
+	DisableIssValidation bool   `json:"disable_iss_validation,omitempty"`
+	UseLocalCaJwt        bool   `json:"use_local_ca_jwt,omitempty"`
+	ClusterAPIType       string `json:"cluster_api_type,omitempty"`
+}
+
+type KubeAuthConfigs struct {
+	K8SAuths []KubeAuthConfig `json:"k8s_auths,omitempty"`
+}
 
 var (
 	akeylessSourceToken      string
@@ -15,6 +41,8 @@ var (
 	destinationGatewayConfigURL    string
 	filterConfigFilePath     string
 )
+
+var timeout = 30000 * time.Millisecond
 
 var rootCmd = &cobra.Command{
 	Use:   "agmigrator",
@@ -41,8 +69,50 @@ var kubernetesCmd = &cobra.Command{
 		if filterConfigFilePath == "" {
 			filterConfigFilePath = getEnvVar("FILTER_CONFIG_FILE_PATH")
 		}
-		// Do Stuff Here
+		run(akeylessSourceToken, akeylessDestinationToken, sourceGatewayConfigURL, destinationGatewayConfigURL, filterConfigFilePath)
 	},
+}
+
+func run(akeylessSourceToken string, akeylessDestinationToken string, sourceGatewayConfigURL string, destinationGatewayConfigURL string, filterConfigFilePath string) {
+	k8sAuthConfigs := lookupK8sAuthConfigs(akeylessSourceToken, sourceGatewayConfigURL)
+	fmt.Println("Found", len(k8sAuthConfigs.K8SAuths), "k8s auth configs")
+	
+}
+
+func lookupK8sAuthConfigs(sourceToken string, sourceGatewayConfigURL string) KubeAuthConfigs {
+	url := sourceGatewayConfigURL + "/configs/k8s-auths"
+
+	var k8sAuthConfigs KubeAuthConfigs
+
+	httpRequestClient := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))
+
+	// Create an http.Request instance
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+
+	bearerToken := "Bearer " + sourceToken
+	req.Header.Add("Authorization", bearerToken)
+	// Call the `Do` method, which has a similar interface to the `http.Do` method
+	res, err := httpRequestClient.Do(req)
+	if err != nil {
+		fmt.Println("Unable to get k8s auth configs:", sourceGatewayConfigURL, err)
+		return generateEmptyK8sAuthConfigs()
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	err2 := json.Unmarshal(body, &k8sAuthConfigs)
+	if err2 != nil {
+		fmt.Println(err)
+	}
+
+	return k8sAuthConfigs
+}
+
+func generateEmptyK8sAuthConfigs() KubeAuthConfigs {
+	k8sAuthConfigs := KubeAuthConfigs{
+		K8SAuths: []KubeAuthConfig{},
+	}
+	return k8sAuthConfigs
 }
 
 func getEnvVar(name string) string {
